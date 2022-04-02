3862a183a5f21acc9e4883510552ec58efa2acae
Poisenous-Puppy <102829424+Poisenous-Puppy@users.noreply.github.com>
2022-04-01 17:57:50 -0700

    Create animal.py

---
+++ animal.py
@@ -0,0 +1,6 @@
+# Author: Michael Chambers
+# GitHub username: poisenous-puppy
+# Date: 1/1/1970
+# Description: What is your favorite animal?
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
